2f7fd233b691dc2b0dc6e8858455e09307cd9377
FAAC <117797535+AdrianAlba@users.noreply.github.com>
2025-04-18 14:28:16 -0600

    prueba2

--- server.py
+++ server.py
@@ -95,19 +95,22 @@
     
 #     asyncio.run(main())
 
-from pymongo import MongoClient
-
-# Conectar a la base de datos MongoDB
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+uri = "mongodb+srv://adrianalba:tukIweCey0ZrOih9@hydroplastdb.rxpa4k9.mongodb.net/?retryWrites=true&w=majority&appName=hydroplastDB"
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+# Send a ping to confirm a successful connection
 try:
-    client = MongoClient('mongodb://localhost:27017/')
-    print("Conexión a MongoDB exitosa")
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
 except Exception as e:
-    print(f"Error al conectar a MongoDB: {e}")
+    print(e)
 
 # Crear el documento con los datos del sensor
 datos_sensor = {
     "timestamp": "2025-04-18T15:42:10Z",
-    "temperatura": 25.87,
+    "temperatura": 69,
     "iluminancia": 810.35,
     "nivelAgua": 78.20,
     "ledRojo": 128,
